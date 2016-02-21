62ffa6ed372a9aa8a4ef90bf2422693c18b1d07b
Medhavi Mahansaria <mahansaria.m@husky.neu.edu>
2016-02-20 23:01:24 -0500

    Sat Feb 20 23:01:23 EST 2016: fixed the sidebar

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -1,3 +1,23 @@
-/**
- * Created by Medhavi on 2/20/16.
- */
+(function (){
+    angular
+        .module("FormBuilderApp")
+        .factory("UserService", UserService);
+
+    function UserService(){
+        var userArr = [
+            {	"_id":123, "firstName":"Alice",            "lastName":"Wonderland",
+                "username":"alice",  "password":"alice",   "roles": ["student"]		},
+            {	"_id":234, "firstName":"Bob",              "lastName":"Hope",
+                "username":"bob",    "password":"bob",     "roles": ["admin"]		},
+            {	"_id":345, "firstName":"Charlie",          "lastName":"Brown",
+                "username":"charlie","password":"charlie", "roles": ["faculty"]		},
+            {	"_id":456, "firstName":"Dan",              "lastName":"Craig",
+                "username":"dan",    "password":"dan",     "roles": ["faculty", "admin"]},
+            {	"_id":567, "firstName":"Edward",           "lastName":"Norton",
+                "username":"ed",     "password":"ed",      "roles": ["student"]		}
+        ];
+
+
+
+    }
+})();
