4a03f1ee8d24091a6420bb987eb86418bcaaeba4
Medhavi Mahansaria <mahansaria.m@husky.neu.edu>
2016-03-18 20:42:41 -0400

    Fri Mar 18 20:42:41 EDT 2016: rhcloud

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
+
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer());
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-//var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var port = 80 || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
+
+app.get('/hello', function(req, res){
+    res.send('hello world');
+});
+
+// for services
 
 require("./public/assignment/server/app.js")(app);
-app.listen(port, ipaddress);
+
+
+app.listen(port, ipaddress, function() {
+ console.log("May the Force be with you!!");
+ });
