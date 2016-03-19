1463c2780293633aa91299c75025bb68ddfd77b7
Medhavi Mahansaria <mahansaria.m@husky.neu.edu>
2016-03-18 20:25:53 -0400

    sss

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ app.use(multer());
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
+
 
 require("./public/assignment/server/app.js")(app);
 app.listen(port, ipaddress);
