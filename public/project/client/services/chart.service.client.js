9f9c8059dfcca8e71a810f6e5d010bab9bd8c4ed
Medhavi Mahansaria <mahansaria.m@husky.neu.edu>
2016-04-28 01:05:19 -0400

    Thu Apr 28 01:05:19 EDT 2016: top chart count 20

--- public/project/client/services/chart.service.client.js
+++ public/project/client/services/chart.service.client.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
-    var TOP_ARTIST_URL = "http://ws.audioscrobbler.com/2.0/?api_key=660fbf2c8a0c4658c2dd385ac4273bc2&format=json&method=chart.gettopartists&limit=15";
-    var TOP_TRACK_URL = "http://ws.audioscrobbler.com/2.0/?api_key=660fbf2c8a0c4658c2dd385ac4273bc2&format=json&method=chart.gettoptracks&limit=15";
+    var TOP_ARTIST_URL = "http://ws.audioscrobbler.com/2.0/?api_key=660fbf2c8a0c4658c2dd385ac4273bc2&format=json&method=chart.gettopartists&limit=20";
+    var TOP_TRACK_URL = "http://ws.audioscrobbler.com/2.0/?api_key=660fbf2c8a0c4658c2dd385ac4273bc2&format=json&method=chart.gettoptracks&limit=20";
 
     angular
         .module("PrismaticMusicApp")
